180c6ff8f6f7f0c83cdf8ef3b09713dfb6a1c679
Anant Kumar Singh <anantkumarsingh5000@gmail.com>
2025-12-10 02:14:32 +0530

    update server startup with DB connection checks

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,19 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+console.log("🔄 Environment variables loaded");
+import server from "./app/app.js";
 
-console.log('🔄 Environment variables loaded');
-
-import express from 'express';
-
-import app from './app/app.js';
-
-import dbconnect from './db/db.js';
+import dbconnect from "./db/db.js";
 const PORT = process.env.PORT || 3000;
-console.log('🔌 Attempting to connect to DB...');
-dbconnect().then(()=>{
+console.log("🔌 Attempting to connect to DB...");
+dbconnect()
+  .then(() => {
     console.log("Database connected successfully");
-    app.listen(PORT, () => {
-        
-        console.log(`Server is running on port ${PORT}`)})
-    
-}).catch((err) => {
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
     console.error("Failed to connect to the database:", err);
     process.exit(1); // Exit the process if database connection fails
-});
-// Serve static files from the public directory
+  });
