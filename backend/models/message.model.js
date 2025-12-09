7ce80a0506faebbc34484a7a2114c001a0202a62
Anant Kumar Singh <anantkumarsingh5000@gmail.com>
2025-12-10 01:41:35 +0530

    update message model to add new fields and refine schema definitions

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -1,50 +1,78 @@
-const messageschema=new mongoose.Schema({
-    sender:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
-    },
- 
-    content:{
-        type:String,
-        required:true
-    },
-    fileurl:{
-        type:String
-    },
-    fileType:{
-        type:String
-    },
-    mediaSize:{
-        type:Number
-    },
-    pageCount:{
-        type:Number
-    },
-    messageType:{
-        type:String,
-        required:true,
-        default:'text'
-    },
-    chat:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Chat",
-        required:true
-    },
-    recievedBy:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
-    }],
-    readBy:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
-    }],
+import { type } from "os";
+import mongoose from "mongoose";
+import User from "./user.model.js";
+import Chat from "./chat.model.js";
 
-    seenStatus:{
-        type:Number,
-        default:0
+const messageschema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    timestamps:true
-});
-const Message=mongoose.model("Message",messageschema);
+    content: {
+      type: String,
+    },
+    fileUrl: {
+      type: String,
+    },
+    fileType: {
+      type: String,
+    },
+    mediaSize: {
+      type: Number,
+    },
+    pageCount: {
+      type: Number,
+    },
+    messageType: {
+      type: String,
+      required: true,
+      default: "text",
+    },
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    recievedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    readBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    publicId: {
+      type: String,
+    },
+    seenStatus: {
+      type: String,
+      default: "sent",
+    },
+
+    isDownloaded: {
+      type: Array,
+      default: [],
+    },
+    originalFileName: {
+      type: String,
+      default: "",
+    },
+    latestMessageType: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Message = mongoose.model("Message", messageschema);
 export default Message;
