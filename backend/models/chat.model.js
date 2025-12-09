4a38201c702b5d9b56c3901b9212feaf09f911ac
Anant Kumar Singh <anantkumarsingh5000@gmail.com>
2025-12-10 01:40:19 +0530

    update chat model to add new fields and refine schema definitions

--- backend/models/chat.model.js
+++ backend/models/chat.model.js
@@ -1,39 +1,65 @@
-const chatschema=new mongoose.Schema({
-    users:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
-    }],
-    unreadCountPerUser:{
-        type:Object
-    },
-    latestMessage:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Message"
-    },
-    isGroupChat:{
-        type:Boolean,
-        default:false
-    },
-    chatImage:{
-        type:String,
-        default:`http://localhost:${process.env.PORT}/static/groupImage.png`
-    },
-    chatName:{
-        type:String
-    },
-    admin:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
-    },
-    chatname:{
-        type:String
-    },groupprofile:{
-        type:String,
-    }
-},{
-    timestamps:true
+import mongoose from "mongoose";
+const chatschema = new mongoose.Schema(
+  {
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
 
-})
-const Chat=mongoose.model("Chat",chatschema);
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    messages: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+      },
+    ],
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    chatImage: {
+      type: String,
+      default: `http://localhost:${process.env.PORT}/static/groupImage.png`,
+    },
+    chatImageForPrivateChat: {
+      type: Array,
+    },
+    chatName: {
+      type: String,
+    },
+    chatNameForPrivateChat: {
+      type: Array,
+    },
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    chatname: {
+      type: String,
+    },
+    groupprofile: {
+      type: String,
+    },
+    groupChatAction: {
+      type: String,
+    },
+    groupDescription: {
+      type: String,
+      default: " ",
+    },
+    groupMembers: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Chat = mongoose.model("Chat", chatschema);
 export default Chat;
