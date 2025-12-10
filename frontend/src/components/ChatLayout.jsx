79f102af84e18184ba390001e2670c8f07cd097a
Anant Kumar Singh <anantkumarsingh5000@gmail.com>
2025-12-10 21:48:22 +0530

    add ChatLayout with Outlet and Footer

---
+++ frontend/src/components/ChatLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import React from "react";
+import Footer from "./Footer";
+
+export default function ChatLayout() {
+  return (
+    <div
+      className="chat-layout"
+      style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+    >
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
